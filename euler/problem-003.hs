9e6102ca2128965168c21dcd740e16e86b57dbab
vagrant <vagrant@precise64.(none)>
2014-07-31 18:10:35 +0000

    third problem added

---
+++ euler/problem-003.hs
@@ -0,0 +1,11 @@
+module Problem003 where
+
+main =
+  print $ reduce 600851475143 primes
+
+reduce n (p:ps) = if n ==  p then p
+                  else if n `rem` p == 0 then reduce (n `div` p) (p:ps)
+                                         else reduce n ps
+
+primes = 2 : 3 : filter (f primes) [5, 7 ..]
+  where f (p:ps) n = p * p > n || n `rem` p /= 0 && f ps n 
