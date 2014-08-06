1268798d93d64d02989e14a89f0fcc160f8ca2d2
vagrant <vagrant@precise64.(none)>
2014-08-06 20:09:49 +0000

    problem 44 solved

--- euler/problem-044.hs
+++ euler/problem-044.hs
@@ -3,15 +3,16 @@ module Problem044 where
 import qualified Data.Set as Set
 
 main =
-  print $ concatMap check $ map fst pents 
+  print $ head $ concatMap check $ map fst pents 
 
-check d = [(fst c, d) | c <- takeWhile (\i -> snd i <= d) pents,
-                        let x = (fst c) + d,
-                        let y = x + d,
-                        isPent x && isPent y]
+check d = [(c, x, d) | c <- cs,
+                       let x = c + d,
+                       let y = x + c,
+                       isPent x && isPent y]
+  where cs = map fst $ takeWhile (\i -> snd i <= d) pents
 
-isPent = (`Set.member` Set.fromList (map fst pents))
+isPent = (`Set.member` Set.fromAscList (map fst pents))
 
-pents = map (\n -> (n * (3 * n - 1) `div` 2, 3 * n + 1)) [1..5000]
+pents = map (\n -> (n * (3 * n - 1) `div` 2, 3 * n + 1)) [1..10000]
 
 
