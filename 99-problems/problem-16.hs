6a01d1eb4facb5634604875933631d47e5d8f340
vagrant <vagrant@precise64.(none)>
2014-08-09 21:46:15 +0000

    removed division test as not really required

--- 99-problems/problem-16.hs
+++ 99-problems/problem-16.hs
@@ -6,5 +6,5 @@ main =
 dropEvery as n = dropEvery' as 1
   where dropEvery' [] _ = []
         dropEvery' (a:as) i
-          | i `rem` n == 0 = dropEvery' as (i + 1)
-          | otherwise      = a : dropEvery' as (i + 1)
+          | i == n    = dropEvery' as 1
+          | otherwise = a : dropEvery' as (i + 1)
