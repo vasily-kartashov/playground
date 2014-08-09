d58b592c4de16003f29ace1f92502e06e22aec5d
vagrant <vagrant@precise64.(none)>
2014-08-09 20:49:58 +0000

    better use of recursion stack

--- 99-problems/problem-08.hs
+++ 99-problems/problem-08.hs
@@ -3,8 +3,8 @@ module Problem08 where
 main = do
   putStrLn $ show $ compress "aaaabccaadeeee"
 
-compress (a:as) = compress' a (as) [a]
-  where compress' p [] cs = reverse cs
-        compress' p (a:as) cs
-          | p == a    = compress' p as cs
-          | otherwise = compress' a as (a:cs) 
+compress (a:as) = compress' a (as)
+  where compress' p [] = [p]
+        compress' p (a:as)
+          | p == a    = compress' p as
+          | otherwise = p : compress' a as 
