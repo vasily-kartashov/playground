40a098d75889a167ded8ac10a9758dd4e70a631b
vagrant <vagrant@precise64.(none)>
2014-08-09 20:51:01 +0000

    edge case covered2

--- 99-problems/problem-08.hs
+++ 99-problems/problem-08.hs
@@ -3,6 +3,7 @@ module Problem08 where
 main = do
   putStrLn $ show $ compress "aaaabccaadeeee"
 
+compress [] = []
 compress (a:as) = compress' a (as)
   where compress' p [] = [p]
         compress' p (a:as)
