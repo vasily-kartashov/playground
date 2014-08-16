5f84c0486523f7429275c80ac5b43b59682d103a
vagrant <vagrant@precise64.(none)>
2014-08-16 19:09:46 +0000

    problem 60 solved

---
+++ euler/problem-060.hs
@@ -0,0 +1,36 @@
+module Problem060 where
+
+main = 
+  print $ findExpansion 5 5
+
+type Group = (Int, [Int])
+type Tree = (Int, [Group])
+
+findExpansion :: Int -> Int -> [[Int]]
+findExpansion lb ub = map snd $ filter (\g -> fst g >= lb) gs
+   where gs = snd $ head $  dropWhile (\t -> fst t < ub) $ scanl expand (0, []) primes
+
+expand :: Tree -> Int -> Tree
+expand (n, gs) p = (max n n', (1, [p]) : gs ++ gs')
+  where gs' = map (expandGroup p) $ filter (adjGroup p) gs       
+        n' = let ls = map fst gs' in
+             if length ls == 0 then 0
+                               else maximum ls       
+
+expandGroup :: Int -> Group -> Group
+expandGroup p g = (1 + fst g, p : snd g)
+
+adjGroup :: Int -> Group -> Bool
+adjGroup p g = all (adj p) (snd g)
+
+adj :: Int -> Int -> Bool
+adj a b = let a' = show a
+              b' = show b in
+          isPrime (read (a' ++ b')) && isPrime (read (b' ++ a'))
+
+isPrime :: Int -> Bool
+isPrime x = isPrime' primes x
+  where isPrime' (p:ps) x = p * p > x || x `rem` p /= 0 && isPrime' ps x
+
+primes :: [Int]
+primes = 2 : 3 : filter isPrime [5, 7 ..]
