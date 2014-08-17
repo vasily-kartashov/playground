f605191bebe14e2073f2e9b2dcfb9351f0c5fe66
vagrant <vagrant@precise64.(none)>
2014-08-17 17:11:01 +0000

    problem 67, same as 18

---
+++ euler/problem-067.hs
@@ -0,0 +1,7 @@
+module Problem067 where
+
+main = do s <- readFile "problem-067.txt"
+          let c = (map . map) (read :: String -> Int) $ map words $ lines s
+          print $ foldr1 f c
+          where f xs ys = zipWith3 g xs ys (tail ys)
+                g x y z = x + max y z
