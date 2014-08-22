fee914ca0ab9ce0407bbb6fb7b66a9556e31c6b6
vagrant <vagrant@precise64.(none)>
2014-08-22 15:17:57 +0000

    rewrote 71 a bit

--- euler/problem-071.hs
+++ euler/problem-071.hs
@@ -1,15 +1,21 @@
 module Problem071 where
 
-import Data.Ratio
-import Data.List
+data Ratio = Ratio Int Int
+
+instance Eq Ratio where
+  (Ratio a b) == (Ratio c d) = (a * d) == (b * c)
+
+instance Ord Ratio where
+  (Ratio a b) `compare` (Ratio c d) = (b * c) `compare` (a * d)
+
+instance Show Ratio where
+  show (Ratio a b) = show (a `div` f) ++ "/" ++ show (b `div` f)
+                     where f = gcd a b
 
 main = 
-  print $ frac 3 7 (10 ^ 6)
+  print $ minimum $ filter (> x) $ map (nom x) [2 .. (10 ^ 6)] 
+  where x = Ratio 3 7
 
-frac a b n = frac' 1 0 2  n
-  where frac' s r l q
-          | q >= l = let p = (a * q - 1) `div` b in
-                     if p * s > r * q then frac' q p (s `div` (a * s - b * r)) (q - 1)
-                                      else frac' s r l (q - 1)
-          | otherwise = let f = gcd r s in
-                        (r `div` f, s `div` f)
+nom :: Ratio -> Int -> Ratio
+nom (Ratio a b) q = let p = (a * q - 1) `div` b in Ratio p q
+
