2e0caf2277b0001f37e2e37bf835f330d345aa02
vagrant <vagrant@precise64.(none)>
2014-08-25 13:42:07 +0000

    use proper libraries

--- provision/install.sh
+++ provision/install.sh
@@ -6,10 +6,13 @@ sudo apt-get install python-software-properties -y
 sudo add-apt-repository ppa:cassou/emacs -y
 sudo apt-get update
 
-sudo apt-get install ghc emacs24 git make texinfo cabal-install hlint -y
+sudo apt-get install ghc emacs24 git make texinfo cabal-install hlint llvm -y
 
 sudo git clone https://github.com/haskell/haskell-mode.git /home/vagrant/haskell-mode
 cd /home/vagrant/haskell-mode
 sudo make all
+
+sudo ln -s /usr/bin/opt /usr/bin/opt-3.0
+sudo ln -s /usr/bin/llc /usr/bin/llc-3.0
 
 sudo cp /vagrant/provision/.emacs /home/vagrant
