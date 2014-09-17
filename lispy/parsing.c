9b60f4ae95e8402208d9ba47badd348310eac438
vagrant <vagrant@precise64.(none)>
2014-09-17 19:33:43 +0000

    evaluation added

--- lispy/parsing.c
+++ lispy/parsing.c
@@ -3,22 +3,39 @@
 #include <editline/readline.h>
 #include <editline/history.h>
 
+#define ONOP(a,o,c) if (strcmp((a),(o)) == 0) { return (c);}
+
+long eval(mpc_ast_t* t) {
+
+  if (strstr(t->tag, "num")) {
+    return atoi(t->contents);
+  }
+  char* op = t->children[2]->contents;
+  long x = eval(t->children[1]);
+  long y = eval(t->children[3]);
+
+  ONOP(op, "+", x + y);
+  ONOP(op, "-", x - y);
+  ONOP(op, "*", x * y);
+  ONOP(op, "/", x / y);
+  ONOP(op, "%", x % y);
+
+  return 0;
+}
+
 int main(int argc, char** argv) {
 
   mpc_parser_t* Number     = mpc_new("num");
   mpc_parser_t* Operator   = mpc_new("op");
   mpc_parser_t* Expression = mpc_new("exp");
-  mpc_parser_t* Language   = mpc_new("lang");
   
-  /* Define them with the following Language */
   mpca_lang(MPCA_LANG_DEFAULT,
     "                                            \
       num  : /-?[0-9]+(\\.[0-9]+)?/ ;            \
       op   : '+' | '-' | '*' | '/' | '%' ;       \
       exp  : <num> | '(' <exp> <op> <exp> ')' ;  \
-      lang : /^/ <exp> <op> <exp> /$/ ;  \
     ",
-    Number, Operator, Expression, Language);
+    Number, Operator, Expression);
   
   puts("Lispy Version 0.0.0.0.2");
   puts("Press Ctrl+c to Exit\n");
@@ -28,14 +45,12 @@ int main(int argc, char** argv) {
     char* input = readline("lispy> ");
     add_history(input);
     
-    /* Attempt to parse the user input */
     mpc_result_t r;
-    if (mpc_parse("<stdin>", input, Language, &r)) {
-      /* On success print and delete the AST */
-      mpc_ast_print(r.output);
+    if (mpc_parse("<stdin>", input, Expression, &r)) {
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
     } else {
-      /* Otherwise print and delete the Error */
       mpc_err_print(r.error);
       mpc_err_delete(r.error);
     }
@@ -43,8 +58,6 @@ int main(int argc, char** argv) {
     free(input);
   }
   
-  /* Undefine and delete our parsers */
-  mpc_cleanup(4, Number, Operator, Expression, Language);
-  
+  mpc_cleanup(4, Number, Operator, Expression);
   return 0;
 }  
