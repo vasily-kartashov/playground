008518d24eae30ae341d7bfafc3a3f033683d1c6
vagrant <vagrant@precise64.(none)>
2014-09-22 16:19:42 +0000

    assign statement added

--- computation/simple.rb
+++ computation/simple.rb
@@ -86,6 +86,50 @@ class Multiply < Struct.new(:left, :right)
 
 end
 
+class DoNothing
+
+  def to_s
+    'do-nothing'
+  end
+
+  def inspect
+    "#{self}"
+  end
+ 
+  def ==(other_statement)
+    other_statement.instance_of?(DoNothing)
+  end
+
+  def reducible?
+    false
+  end
+
+end
+
+class Assign < Struct.new(:name, :expression)
+
+  def to_s
+    "#{name} = #{expression}"
+  end
+
+  def inspect
+    "#{self}"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    if expression.reducible?
+      [Assign.new(name, expression.reduce(environment)), environment]
+    else
+      [DoNothing.new, environment.merge({ name => expression })]
+    end
+  end
+
+end
+
 class Machine < Struct.new(:expression, :environment)
 
   def run
