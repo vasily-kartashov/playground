43f0956379e38f2aca5a0fef6f4e249647b7d0cf
vagrant <vagrant@precise64.(none)>
2014-09-25 13:14:32 +0000

    simple dfa

---
+++ computation/example4.rb
@@ -0,0 +1,13 @@
+load 'dfa.rb'
+
+rulebook = DFARulebook.new([
+  FARule.new(1, 'a', 2), FARule.new(1, 'b', 1),
+  FARule.new(2, 'a', 2), FARule.new(2, 'b', 3),
+  FARule.new(3, 'a', 3), FARule.new(3, 'b', 3)
+])
+
+puts rulebook.next_state(1, 'a')
+puts rulebook.next_state(1, 'b')
+puts rulebook.next_state(2, 'b')
+
+
