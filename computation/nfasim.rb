a8c1e9f68e8ccc6358bcc5f1c28f6f8d1c8078fd
vagrant <vagrant@precise64.(none)>
2014-09-29 10:21:53 +0000

    sim removed

--- computation/nfasim.rb
+++
@@ -1,9 +0,0 @@
-load 'freemoves.rb'
-
-class NFASimulation < Struct.new(:nfa_design)
-  def next_state(state, character)
-    nfa_design.to_nfa(state).tap { |nfa|
-      nfa.read_character(character)
-    }.current_states
-  end
-end
