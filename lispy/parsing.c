13aaf5ba895368507afc401aa2e749de0b123480
vagrant <vagrant@precise64.(none)>
2014-10-03 18:36:00 +0000

    chapter 7 done

--- lispy/parsing.c
+++ lispy/parsing.c
@@ -5,37 +5,44 @@
 
 #define ONOP(a,o,c) if (strcmp((a),(o)) == 0) { return (c);}
 
-long eval(mpc_ast_t* t) {
-
-  if (strstr(t->tag, "num")) {
-    return atoi(t->contents);
-  }
-  char* op = t->children[2]->contents;
-  long x = eval(t->children[1]);
-  long y = eval(t->children[3]);
-
+long eval_op(long x, char* op, long y) {
   ONOP(op, "+", x + y);
   ONOP(op, "-", x - y);
   ONOP(op, "*", x * y);
   ONOP(op, "/", x / y);
   ONOP(op, "%", x % y);
-
   return 0;
+}
+
+long eval(mpc_ast_t* t) {
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+  char* op = t->children[1]->contents;
+  long x = eval(t->children[2]);
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t -> children[i]));
+    i++;
+  }
+  return x;
 }
 
 int main(int argc, char** argv) {
 
-  mpc_parser_t* Number     = mpc_new("num");
-  mpc_parser_t* Operator   = mpc_new("op");
-  mpc_parser_t* Expression = mpc_new("exp");
+  mpc_parser_t* Number = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr = mpc_new("expr");
+  mpc_parser_t* Lispy = mpc_new("lispy");
   
   mpca_lang(MPCA_LANG_DEFAULT,
-    "                                            \
-      num  : /-?[0-9]+(\\.[0-9]+)?/ ;            \
-      op   : '+' | '-' | '*' | '/' | '%' ;       \
-      exp  : <num> | '(' <exp> <op> <exp> ')' ;  \
+    "                                                    \
+      number   : /-?[0-9]+/ ;                            \
+      operator : '+' | '-' | '*' | '/' | '%' ;           \
+      expr     : <number> | '(' <operator> <expr>+ ')' ; \
+      lispy    : /^/ <operator> <expr>+ /$/ ;            \
     ",
-    Number, Operator, Expression);
+    Number, Operator, Expr, Lispy);
   
   puts("Lispy Version 0.0.0.0.2");
   puts("Press Ctrl+c to Exit\n");
@@ -46,7 +53,7 @@ int main(int argc, char** argv) {
     add_history(input);
     
     mpc_result_t r;
-    if (mpc_parse("<stdin>", input, Expression, &r)) {
+    if (mpc_parse("<stdin>", input, Lispy, &r)) {
       long result = eval(r.output);
       printf("%li\n", result);
       mpc_ast_delete(r.output);
@@ -58,6 +65,6 @@ int main(int argc, char** argv) {
     free(input);
   }
   
-  mpc_cleanup(4, Number, Operator, Expression);
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
   return 0;
 }  
